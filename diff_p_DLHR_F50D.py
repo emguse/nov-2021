24dcd1e1d037d229c667a160b47835f9d84d6b37
emguse <30695541+emguse@users.noreply.github.com>
2021-11-07 16:51:51 +0900

    Mainly adjusting comments.

--- diff_p_DLHR_F50D.py
+++ diff_p_DLHR_F50D.py
@@ -2,9 +2,9 @@ from smbus2 import SMBus
 import time
 
 '''
-# 2021/11/05 ver.1.01
-# Author : emguse
-# License: MIT License
+- 2021/11/07 ver.1.02
+- Author : emguse
+- License: MIT License
 '''
 
 ADDRESS = 0x29
@@ -13,7 +13,6 @@ OS_DIG = 0.5 * 2 ** 24 # For Differential Operating Range sensors
 FSS_IN_H2O = 2 * 0.5 # For Differential Operating Range sensors : 2 x Full Scale Pressure.
 PA_CONVERSION = 249.089 # Conversion factor from "inH2O" to "Pa"
 FSS_PA = FSS_IN_H2O * PA_CONVERSION # FSS/2 for "Pa"
-ZERO_OFFSET = 2 # Zero point correction
 
 START_SINGLE = 0xAA
 START_AVERAGE2 = 0xAC
@@ -21,10 +20,10 @@ START_AVERAGE4 = 0xAD
 START_AVERAGE8 = 0xAE
 START_AVERAGE16 = 0xAF
 
-class DLHR_F50D():
+class DifferentialPressureSensorDLHR_F50D():
     '''
-    # All sensoers (Amphenol) DLHR-F50D-E1BD-C-NAV8
-    # for Raspberry Pi
+    ### Class for using All sensoers (Amphenol) DLHR-F50D-E1BD-C-NAV8 with Raspberry Pi
+    For simple use, you can instantiate it and then call the method "read_p()" to get the differential pressure in the return value.
     '''
     def __init__(self) -> None:
         self.address = ADDRESS
@@ -41,10 +40,11 @@ class DLHR_F50D():
         self.bus = SMBus(1)
         pass
 
-    def send_start(self):
+    def send_start(self) -> None:
+        '''Send Measurement Commands to the sensor when called'''
         self.bus.write_byte(self.address, START_AVERAGE4)
     
-    def status_read(self):
+    def status_read(self) -> None:
         self.status = self.bus.read_byte(self.address)
         if self.status & 0x40 == 0x40:
             self.power = True
@@ -54,7 +54,7 @@ class DLHR_F50D():
             self.busy = True
         else:
             self.busy = False
-        self.mode = (self.status and 0x18) >> 3
+        self.mode = (self.status & 0x18) >> 3
         if self.status & 0x04 == 0x04:
             self.memory_error = True
         else:
@@ -65,6 +65,11 @@ class DLHR_F50D():
             self.alu_error = False
 
     def read_busy(self) -> bool:
+        '''
+        ### After reading the status register, it retrieves only the Busy status from the result and returns the result.
+        Return:
+            Sensor busy status (bool)
+        '''
         self.status = self.bus.read_byte(self.address)
         if self.status & 0x20 == 0x20:
             self.busy = True
@@ -73,37 +78,54 @@ class DLHR_F50D():
         return self.busy
     
     def poll_busy(self):
+        '''Keep reading the status until Busy becomes False.'''
         while self.read_busy():
             pass
     
-    def correction_p(self):
+    def correction_p(self) -> None:
+        '''Returns the raw pressure value as a translated pressure value.'''
         self.pressure = 1.25 * ((self.rawp - OS_DIG)/ 2**24) * FSS_PA
 
-    def correction_t(self):
+    def correction_t(self) -> None:
+        '''Returns the raw temperature value as a translated temperature value.'''
         self.temprature = (self.rawt * 125)/ 2**24 - 40
 
     def read(self):
+        '''Get the raw value from the sensor.'''
         data = self.bus.read_i2c_block_data(self.address, 0x00, 7)
         self.rawp = (data[1] << 16) | (data[2] << 8) | data[3]
         self.rawt = (data[4] << 16) | (data[5] << 8) | data[6]
 
-    def read_p(self):
+    def read_p(self) -> float:
+        '''
+        ### Get the corrected differential pressure
+        Return:
+            Corrected differential pressure
+        '''
         self.send_start()
         self.poll_busy()
         self.read()
         self.correction_p()
+        return self.pressure
     
-    def read_t(self):
+    def read_t(self) -> float:
+        '''
+        ### Get the corrected temprature
+        Return:
+            Corrected temprature
+        '''
         self.send_start()
         self.poll_busy()
         self.read()
         self.correction_t()
+        return self.temprature
 
 def main(): # Sample usage
-    dlhr_f50d = DLHR_F50D()
+    ZERO_OFFSET = 2 # Zero point correction
+    dlhr_f50d = DifferentialPressureSensorDLHR_F50D()
     while True:
-        dlhr_f50d.read_p()
-        print("pressure :" + str(round(dlhr_f50d.pressure - ZERO_OFFSET ,4)))
+        p = dlhr_f50d.read_p()
+        print("pressure :" + str(round(p - ZERO_OFFSET ,4)))
         time.sleep(1)
 
 if __name__ == '__main__':
