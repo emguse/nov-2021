46328b2672618dd7f07cc7754850af4ce7e6e092
emguse <30695541+emguse@users.noreply.github.com>
2021-11-16 21:23:40 +0900

    Checked the operation of the actual sensor.

--- diff_p_D6F_PH0505.py
+++ diff_p_D6F_PH0505.py
@@ -2,10 +2,10 @@ from smbus2 import SMBus
 import time
 
 '''
-# 2021/11/07 ver.0.03
-# Author : emguse
-# License: MIT License
-# Note: This code does not work yet. It has not been tested with a real sensor.
+- 2021/11/16 ver.0.10
+- Author : emguse
+- License: MIT License
+- Attention! Please set to False as we have not yet confirmed the operation of the CRC!
 '''
 
 I2C_ADDRES = 0x6C # i2c addres
@@ -16,21 +16,21 @@ RANGE_1000 = 1000 # ±500Pa(D6F-PH5050)
 RANGE_250 = 250 # 0-250Pa(D6F-PH0025)
 
 def CRC(d):
-  crc = 0xff
-  for s in d:
-    crc ^= s
-    for i in range(8):
-      if crc & 0x80:
-        crc <<= 1
-        crc ^= 0x131
-      else:
-        crc <<= 1
-  return crc
+    crc = 0xFF
+    for s in d:
+        crc ^= s
+        for _ in range(8):
+            if crc & 0x80:
+                crc <<= 1
+                crc ^= 0x131
+        else:
+            crc <<= 1
+    print(hex(crc))
+    return crc
 
 class D6F_PH0505():
     '''
-    # OMRON D6F-PH0505 Differential pressure sensor
-    # for Raspberry Pi
+    ### Class for using OMRON D6F-PH0505 Differential pressure sensor with Raspberry Pi
     '''
     def __init__(self) -> None:
         self.d6f_addres = I2C_ADDRES
@@ -40,34 +40,27 @@ class D6F_PH0505():
         self.initializing()
         if CRC_ENABLE == True:
             self.crc_emable()
-        pass
-
     def initializing(self):
         self.bus.write_byte_data(self.d6f_addres, 0x0B, 0x00)
-
     def crc_emable(self):
         d = [0xD0, 0x49, 0x18, 0x02]
         self.bus.write_i2c_block_data(self.d6f_addres, 0x00, d)
-    
     def start_measurement(self):
         d = [0xD0 ,0x40 ,0x18, 0x06]
         self.bus.write_i2c_block_data(self.d6f_addres, 0x00, d)
-        #Always read after 33 milliseconds have elapsed after the command is issued.
-
+        #It will always read 33 milliseconds after the command is issued.
     def read(self):
+        d2 = [0xD0, 0x51, 0x2C]
+        self.bus.write_i2c_block_data(self.d6f_addres, 0x00, d2)
         if CRC_ENABLE == True:
-            d1 = [0xD0, 0x51, 0x3C]
-            self.bus.write_i2c_block_data(self.d6f_addres, 0x00, d1)
             raw = self.bus.read_i2c_block_data(self.d6f_addres, 0x07, 3)
-            if raw[2] != CRC(raw):
+            print(raw)
+            if raw[2] != CRC(raw[:2]):
                 raise RuntimeError("CRC Error!!")
         else:
-            d1 = [0xD0, 0x51, 0x2C]
-            self.bus.write_i2c_block_data(self.d6f_addres, 0x00, d1)
             raw = self.bus.read_i2c_block_data(self.d6f_addres, 0x07, 2)
         pv = raw[0] << 8 | raw[1]
         self.dp = (pv - 1024) / 60000 * RANGE_100 - RANGE_100 / 2
-
     def hw_reset(self):
         self.bus.write_byte_data(self.d6f_addres, 0x00, 0x80)
 
@@ -78,7 +71,7 @@ def main():
         time.sleep(0.033)
         d6f_ph0505.read()
         print("diff_p:" + str(round(d6f_ph0505.dp, 4)))
-        time.sleep(1)
+        time.sleep(0.005)
 
 if __name__ == '__main__':
     main()
