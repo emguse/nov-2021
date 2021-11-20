3cd9d6ac9f3d105044ddeec107e7dc115fa5deb8
emguse <30695541+emguse@users.noreply.github.com>
2021-11-20 22:49:39 +0900

    Added a process to get the value after the trigger and save it to a file.

--- diff_press_logger_a.py
+++ diff_press_logger_a.py
@@ -8,25 +8,26 @@ import csv
 
 SAMPLE_FREQ = 32 # Hz
 SAMPLE_INTERVAL = 1/SAMPLE_FREQ # 
-IVENT_LENGTH = 60 # sec
+IVENT_LENGTH = 1 # sec
 QUE_SIZE = int(IVENT_LENGTH/2 * SAMPLE_FREQ)
 MOVE_AVE_LENGTH = 8
-THRESHOLD = 0.8
+THRESHOLD = 1.0
 MAX_VALUE = 125
 SAVE_DIR = './log/'
 
 ZERO_OFFSET = 1.9 # Zero point correction
 
-def export_csv(d_a,*trg):
+def export_csv(d_a):
     now = datetime.datetime.now()
     now_iso = now.strftime('%Y-%m-%d') + 'T' + now.strftime('%H_%M_%S_%f')
-    filename = str(SAVE_DIR + now_iso)
+    filename = str(SAVE_DIR + now_iso + '.csv')
     try:
         with open(filename, 'w', newline='') as f:
-            writer = csv.writer(f)
+            #writer = csv.writer(f)
             print("Start exporting data")
             for i in d_a:
-                writer.writerow(i)
+                #writer.writerow(i)
+                f.write(str(i)+'\n')
             print("Export Complete")
     except:
         print("File export error")
@@ -37,6 +38,7 @@ def main():
     record_intarval = PollingTimer(1)#IVENT_LENGTH)
     ma = MovingAverage(MOVE_AVE_LENGTH)
     dq_p = deque(maxlen=QUE_SIZE)
+    dq_after = deque(maxlen=QUE_SIZE)
 
     wavesave = WavSave()
     wavesave.set_wav_param(1,2,SAMPLE_FREQ)
@@ -52,13 +54,25 @@ def main():
                 record_intarval.timer_update_only()
                 if record_intarval.up_state == True:
                     record_intarval.up_state = False
-                    #wavesave.save_w_date(dq_p)
-                    #export_csv(dq_p)
+
                     print('Detect!!')
                     now = datetime.datetime.now()
                     print(now.strftime('%Y-%m-%d') + 'T' + now.strftime('%H_%M_%S_%f'))
                     print(ma_p)
                     print(sum(dq_p)/len(dq_p))
+
+                    for i in range(QUE_SIZE):
+                        while True:
+                            read_intarval.timer_update()
+                            if read_intarval.up_state == True:
+                                dlhr_f50d.read_p()
+                                ma_p = ma.simple_moving_average(dlhr_f50d.pressure - ZERO_OFFSET)
+                                dq_after.append(ma_p)
+                                break
+                    ar = list(dq_p)
+                    ar.extend(dq_after)
+                    #wavesave.save_w_date(dq_p)
+                    export_csv(ar)
 
 if __name__ == '__main__':
     main()
