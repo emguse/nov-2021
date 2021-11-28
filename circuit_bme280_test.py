7f95404da870cbe2439ce7d82201db6a3ecf049b
emguse <30695541+emguse@users.noreply.github.com>
2021-11-29 00:40:49 +0900

    Add onboard LED operation

--- circuit_bme280_test.py
+++ circuit_bme280_test.py
@@ -1,17 +1,67 @@
 import board
 import time
 from adafruit_bme280 import basic as adafruit_bme280
+import digitalio
+import neopixel
 
 i2c = board.I2C()
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+pixel_pin = board.NEOPIXEL
+num_pixels = 1
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
+)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+    
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
 
 while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
     print("Altitude = %0.2f meters" % bme280.altitude)
-    time.sleep(2)
+    #time.sleep(2)
+    pixels.fill((255, 0, 0))
+    pixels.show()
+    time.sleep(1)
+    pixels.fill((0, 255, 0))
+    pixels.show()
+    time.sleep(1)
+    pixels.fill((0, 0, 255))
+    pixels.show()
+    time.sleep(1)
+    for i in range(10):
+        rainbow_cycle(0.005)
 
+#print("Hellow, world")
 '''
 Circuit Python demo code for "Adafruit Feather STM32F405 Express" 
 connected to "SparkFun Atmospheric Sensor Breakout - BME280 (Qwiic)" 
